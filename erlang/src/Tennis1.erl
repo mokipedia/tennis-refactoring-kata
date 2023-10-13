090d53b3b60502e23f0ef5671276a296c0f9ec71
Valentin Moser <valentin.moser@ebcont.com>
2023-10-13 10:18:24 +0200

    Initialize erlang

---
+++ erlang/src/Tennis1.erl
@@ -0,0 +1,8 @@
+-module('Tennis1').
+
+-include("Tennis1.hrl").
+-export([score/1]).
+
+-spec score(#game{}) -> string().
+score(#game{player1 = Player1, player2 = Player2, player1_name = P1N, player2_name = P2N}) ->
+  "Love-All".
---
+++ erlang/src/Tennis2.erl
@@ -0,0 +1,9 @@
+
+-module('Tennis2').
+
+-include("Tennis2.hrl").
+-export([score/1]).
+
+-spec score(#game{}) -> string().
+score(#game{player1 = Player1, player2 = Player2, player1_name = P1N, player2_name = P2N}) ->
+  "Love-All".
